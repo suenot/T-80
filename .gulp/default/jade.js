7042327cfcfab7d04af41c3f13c237f322ec8ddd
Eugen Soloviov <suenot@gmail.com>
2016-03-20 13:05:36 +0300

    comment danger jade-inheritance

--- .gulp/default/jade.js
+++ .gulp/default/jade.js
@@ -4,23 +4,23 @@ var plumber = require('gulp-plumber');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var src = {};
-var jadeInheritance = require('gulp-jade-inheritance');
+// var jadeInheritance = require('gulp-jade-inheritance');
 var jade = require('gulp-jade');
-var changed = require('gulp-changed');
-var cached = require('gulp-cached');
-var gulpif = require('gulp-if');
-var filter = require('gulp-filter');
+// var changed = require('gulp-changed');
+// var cached = require('gulp-cached');
+// var gulpif = require('gulp-if');
+// var filter = require('gulp-filter');
 var errorHandler = require('../utils/errorHandler');
 
 gulp.task('jade', function() {
 	return gulp.src(['assets/**/**/**/*.jade'])
 	.pipe(plumber({errorHandler: onError}))
-	.pipe(changed('public', {extension: '.html'}))
-	.pipe(gulpif(global.isWatching, cached('jade')))
-	.pipe(jadeInheritance({basedir: 'assets'}))
-	.pipe(filter(function (file) {
-		return !/\/_/.test(file.path) && !/^_/.test(file.relative);
-	}))
+	// .pipe(changed('public', {extension: '.html'}))
+	// .pipe(gulpif(global.isWatching, cached('jade')))
+	// .pipe(jadeInheritance({basedir: 'assets'}))
+	// .pipe(filter(function (file) {
+	// 	return !/\/_/.test(file.path) && !/^_/.test(file.relative);
+	// }))
 	.pipe(jade({
 		pretty: true,
 		basedir: 'assets'
