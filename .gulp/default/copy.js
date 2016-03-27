4010f661401e40b147c53b1e0dbb40ea5bd3ae1b
Eugen Soloviov <suenot@gmail.com>
2016-03-27 19:57:55 +0300

    fix gif

--- .gulp/default/copy.js
+++ .gulp/default/copy.js
@@ -4,7 +4,7 @@ var browserSync = require('browser-sync');
 var newer = require('gulp-newer');
 
 gulp.task('copy', function() {
-	return gulp.src(['assets/**/*.png', 'assets/**/*.jpg', 'assets/**/*.ico', 'assets/**/*.txt', 'assets/**/*.xml', 'assets/**/*.eot', 'assets/**/*.svg', 'assets/**/*.ttf', 'assets/**/*.woff', 'assets/**/*.woff2', 'assets/**/*.otf', 'assets/**/*.js', 'assets/**/*.css'])
+	return gulp.src(['assets/**/*.png', 'assets/**/*.jpg', 'assets/**/*.gif', 'assets/**/*.ico', 'assets/**/*.txt', 'assets/**/*.xml', 'assets/**/*.eot', 'assets/**/*.svg', 'assets/**/*.ttf', 'assets/**/*.woff', 'assets/**/*.woff2', 'assets/**/*.otf', 'assets/**/*.js', 'assets/**/*.css'])
 	.pipe(newer('public'))
 	.pipe(gulp.dest('public'))
 	.pipe(browserSync.reload({stream: true}));
--- .gulp/default/watch.js
+++ .gulp/default/watch.js
@@ -7,5 +7,5 @@ gulp.task('watch', function() {
 	gulp.watch('assets/**/**/**/*.html', ['nunjucks']);
 	gulp.watch('assets/{css,blocks}/**/**/*.styl', ['styl']);
 	gulp.watch('assets/{css,blocks}/**/**/*.{sass,scss}', ['sass']);
-	gulp.watch(['assets/**/*.png', 'assets/**/*.jpg', 'assets/**/*.ico', 'assets/**/*.txt', 'assets/**/*.xml', 'assets/**/*.eot', 'assets/**/*.svg', 'assets/**/*.ttf', 'assets/**/*.woff', 'assets/**/*.woff2', 'assets/**/*.otf', 'assets/**/*.js', 'assets/**/*.css'], ['copy']);
+	gulp.watch(['assets/**/*.png', 'assets/**/*.jpg', 'assets/**/*.gif', 'assets/**/*.ico', 'assets/**/*.txt', 'assets/**/*.xml', 'assets/**/*.eot', 'assets/**/*.svg', 'assets/**/*.ttf', 'assets/**/*.woff', 'assets/**/*.woff2', 'assets/**/*.otf', 'assets/**/*.js', 'assets/**/*.css'], ['copy']);
 });
