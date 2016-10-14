35e796b1552370979a02f100668b5d33b219b6c2
Eugen Soloviov <suenot@gmail.com>
2016-10-14 07:28:47 +0300

    refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,39 @@
 'use strict';
-
 var runSequence = require('run-sequence');
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-
+var connect = require('gulp-connect');
+var newer = require('gulp-newer');
+var isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
+var rimraf = require('gulp-rimraf');
+var isProduction = process.env.NODE_ENV == 'production';
+var browserSync = require('browser-sync');
+var plumber = require('gulp-plumber');
+var nunjucks = require('gulp-nunjucks-html');
+var config = require('./package.json').config;
+var isHtml = config.template === 'html';
+var isPug = config.template === 'pug';
+var pug = require('gulp-pug');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var gulpif = require('gulp-if');
+var sourcemaps = require('gulp-sourcemaps');
+var prefix = gutil.env.prefix || process.env.NODE_ENV == 'production';
+var rucksack = require('rucksack-css');
+var serverOf = gutil.env.serverof;
+var sftp = require('gulp-sftp');
+var isWebpack = config.webpack === 'true';
+var webpackStream = require('webpack-stream');
+var errorHandler = require('gulp-plumber-error-handler');
+var statsLogger = require('webpack-stats-logger');
+var makeWebpackConfig = require('./webpack.config.js');
 var onError = function(err) {
 	gutil.beep(),
 	gutil.log(gutil.colors.red(err))
 };
 
+// Default
 gulp.task('default', function(cb) {
 	runSequence(
 		'del',
@@ -27,11 +52,7 @@ gulp.task('default', function(cb) {
 	);
 });
 
-
-var connect = require('gulp-connect');
-var newer = require('gulp-newer');
-var isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
-
+// Copy
 gulp.task('copy', function() {
 	if (!isDevelopment) {
 		return gulp.src(['assets/**/*.png', 'assets/**/*.jpg', 'assets/**/*.gif', 'assets/**/*.ico', 'assets/**/*.txt', 'assets/**/*.xml', 'assets/**/*.eot', 'assets/**/*.svg', 'assets/**/*.ttf', 'assets/**/*.woff', 'assets/**/*.woff2', 'assets/**/*.otf', 'assets/**/*.js', 'assets/**/*.css', '!assets/js/*.webpack.js'])
@@ -41,9 +62,7 @@ gulp.task('copy', function() {
 	};
 });
 
-var rimraf = require('gulp-rimraf');
-var isProduction = process.env.NODE_ENV == 'production';
-
+// Del
 gulp.task('del', function() {
 	if (gutil.env.del || isProduction) {
 		return gulp.src(['public/*', '!public/CNAME', '!public/.git'], { read: false })
@@ -51,19 +70,13 @@ gulp.task('del', function() {
 	}
 });
 
-var browserSync = require('browser-sync');
-
+// Livereload
 gulp.task('livereload', function () {
 	return gulp.src(['public/*.html'])
 	.pipe(connect.reload());
 });
 
-
-var plumber = require('gulp-plumber');
-var nunjucks = require('gulp-nunjucks-html');
-var config = require('./package.json');
-var isHtml = config.template === 'html';
-
+// Nunjucks
 gulp.task('nunjucks', function() {
 	if (isHtml) {
 		return gulp.src(['assets/**/**/**/*.html'])
@@ -75,18 +88,13 @@ gulp.task('nunjucks', function() {
 	}
 });
 
-
-
-var isPug = config.template === 'pug';
-var pug = require('gulp-pug');
-
+// Pug
 var pugFiles;
 if (isDevelopment) {
 	pugFiles = ['assets/**/**/**/*.pug', '!assets/blocks/**/**/*.pug', '!assets/{_foot,_head,_layout}.pug'];
 } else {
 	pugFiles = ['assets/**/**/**/*.pug'];
 };
-
 gulp.task('pug', function() {
 	if (isPug) {
 		return gulp.src(pugFiles)
@@ -99,23 +107,13 @@ gulp.task('pug', function() {
 	}
 });
 
-
-
-var sass = require('gulp-sass');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var gulpif = require('gulp-if');
-var sourcemaps = require('gulp-sourcemaps');
-var prefix = gutil.env.prefix || process.env.NODE_ENV == 'production';
-var rucksack = require('rucksack-css');
-
+// Sass
 var sassFiles;
 if (isDevelopment) {
 	sassFiles = 'assets/{css,vendor}/**/**/**/*.{sass,scss}';
 } else {
 	sassFiles = 'assets/{css,blocks,vendor}/**/**/**/*.{sass,scss}';
 };
-
 gulp.task('sass', function () {
 	gulp.src(sassFiles)
 	.pipe(plumber({errorHandler: onError}))
@@ -128,21 +126,17 @@ gulp.task('sass', function () {
 	.pipe(connect.reload());
 });
 
-
-var serverOf = gutil.env.serverof;
+// Server
 var reloadBrowser = true;
-
 if (gutil.env.liveof === true) {
 	reloadBrowser = false;
 };
-
 var root;
 if (isDevelopment) {
 	root = ['public', 'assets'];
 } else {
 	root = 'public';
 };
-
 gulp.task('server', function() {
 	if (!serverOf) {
 		connect.server({
@@ -153,15 +147,12 @@ gulp.task('server', function() {
 	}
 });
 
-
-var sftp = require('gulp-sftp');
-
+// Sftp
 var server = {
 	host: '185.5.250.59',
 	user: 'frontend',
 	remotePath: '/home/frontend/sites/de-core.net'
 }
-
 gulp.task('sftp', function () {
 	return gulp.src('public/**/*')
 	.pipe(sftp({
@@ -170,7 +161,6 @@ gulp.task('sftp', function () {
 		remotePath: server.remotePath
 	}));
 });
-
 gulp.task('sftp-default', ['default'], function () {
 	return gulp.src('public/**/*')
 	.pipe(sftp({
@@ -180,10 +170,7 @@ gulp.task('sftp-default', ['default'], function () {
 	}));
 });
 
-
-
-var isWebpack = config.webpack === 'true';
-
+// watch
 gulp.task('watch', function() {
 	if (!serverOf) {
 		if (isPug) {
@@ -202,18 +189,16 @@ gulp.task('watch', function() {
 	}
 });
 
-var webpackStream = require('webpack-stream');
-var errorHandler = require('gulp-plumber-error-handler');
-var statsLogger = require('webpack-stats-logger');
-var makeWebpackConfig = require('./webpack.config.js');
-// var { NODE_ENV, NOTIFY } = process.env;
-
+// Webpack
+var _process$env = process.env;
+var NODE_ENV = _process$env.NODE_ENV;
+var NOTIFY = _process$env.NOTIFY;
 function runWebpack(watch) {
 	var webpackConfig = makeWebpackConfig({
 		watch,
 		debug: isDevelopment,
 		sourcemaps: isDevelopment,
-		// notify: NOTIFY
+		notify: NOTIFY
 	});
 
 	return gulp
@@ -222,7 +207,6 @@ function runWebpack(watch) {
 		.pipe(webpackStream(webpackConfig, null, statsLogger))
 		.pipe(gulp.dest('public/js'));
 }
-
 gulp.task('webpack', () => {
 	if (!isDevelopment) {
 		return runWebpack(false);
